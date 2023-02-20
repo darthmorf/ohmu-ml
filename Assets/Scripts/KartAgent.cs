77075ddc2ba1d5d408c531418da66bce06672631
Sam Poirier <sampoirier@email.com>
2023-02-20 14:01:40 +0000

    Kart controls

--- Assets/Scripts/KartAgent.cs
+++ Assets/Scripts/KartAgent.cs
@@ -10,12 +10,16 @@ public class KartAgent : MonoBehaviour
 
     // Config Params
     [SerializeField] float speed = 10.0f;
-    [SerializeField] float steerAngle = 30.0f;
+    [SerializeField] float steerSpeed = 5;
+    [SerializeField] float maxSteerAngle = 30.0f;
+    [SerializeField] float brakeTorque = 50.0f;
     [SerializeField] WheelCollider frontL;
     [SerializeField] WheelCollider frontR;
     [SerializeField] WheelCollider backL;
     [SerializeField] WheelCollider backR;
 
+    // State
+    float currentSteerAngle = 0;
 
     void Start()
     {
@@ -29,29 +33,55 @@ public class KartAgent : MonoBehaviour
 
     void DoMovement()
     {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            frontL.brakeTorque = brakeTorque;
+            frontR.brakeTorque = brakeTorque;
+            backL.brakeTorque = brakeTorque;
+            backR.brakeTorque = brakeTorque;
+        }
+        else
+        {
+            frontL.brakeTorque = 0;
+            frontR.brakeTorque = 0;
+            backL.brakeTorque = 0;
+            backR.brakeTorque = 0;
+        }
+        
+        
         if (Keyboard.current.wKey.isPressed)
         {
             frontL.motorTorque = speed;
             frontR.motorTorque = speed;
         }
-
-        if (Keyboard.current.sKey.isPressed)
+        else if (Keyboard.current.sKey.isPressed)
         {
-            frontL.motorTorque = -speed;
-            frontR.motorTorque = -speed;
+            frontL.motorTorque = -speed / 2;
+            frontR.motorTorque = -speed / 2;
         }
+
+        
 
         if (Keyboard.current.aKey.isPressed)
         {
-            frontL.steerAngle = -steerAngle;
-            frontR.steerAngle = -steerAngle;
+            currentSteerAngle -= steerSpeed * Time.deltaTime;
         }
-
-
-        if (Keyboard.current.dKey.isPressed)
+        else if (Keyboard.current.dKey.isPressed)
         {
-            frontL.steerAngle = steerAngle;
-            frontR.steerAngle = steerAngle;
+            currentSteerAngle += steerSpeed * Time.deltaTime;
         }
+        else if (currentSteerAngle < 0)
+        {
+            currentSteerAngle += steerSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentSteerAngle -= steerSpeed * Time.deltaTime;
+        }
+
+        currentSteerAngle = Mathf.Clamp(currentSteerAngle, -maxSteerAngle, maxSteerAngle);
+
+        frontL.steerAngle = currentSteerAngle;
+        frontR.steerAngle = currentSteerAngle;
     }
 }
